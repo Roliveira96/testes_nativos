574d7893ab9e00bcdff6feda9310c5d37be6f73b
Ricardo de Oliveira <ricardo.de.oliveira96@gmail.com>
2019-09-18 21:33:38 -0300

    GeoLocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+
+import { Camera } from '@ionic-native/camera';
 import { MyApp } from './app.component';
+
+
+import { Geolocation } from '@ionic-native/geolocation';
+import { GoogleMaps } from '@ionic-native/google-maps';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -34,7 +41,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    Camera,
+    GoogleMaps,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
+
+
