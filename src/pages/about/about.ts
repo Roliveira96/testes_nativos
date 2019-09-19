69e024f66e54a0c3c630d15f4a905d05a5ee4a5e
Ricardo de Oliveira <ricardo.de.oliveira96@gmail.com>
2019-09-18 21:33:38 -0300

    GeoLocation teste

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,111 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Camera, CameraOptions} from '@ionic-native/camera';
+import { Geolocation } from '@ionic-native/geolocation';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  photo: string = '';
+  resp_coords: any;
+  latitude: any;
+  longitude: any;
+  map: GoogleMap;
 
-  constructor(public navCtrl: NavController) {
+
+  constructor(public navCtrl: NavController, private camera: Camera,private geolocation: Geolocation
+  ) {
 
   }
 
+  loadMap() {
+
+    let api_key = 'AIzaSyB5kxJ96SiNeyfJFtxzomK16EOEqFjqtVY'
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': api_key,
+      'API_KEY_FOR_BROWSER_DEBUG': api_key
+    });
+
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: this.latitude,
+          lng: this.longitude
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    }
+    this.map = GoogleMaps.create('map', mapOptions);
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Eu',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: this.latitude,
+        lng: this.longitude
+      }
+    });
+    marker.showInfoWindow();
+  }
+
+
+
+  takePicture() {
+    this.photo = '';
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: true,
+      targetWidth: 100,
+      targetHeight: 100
+    }
+
+    this.camera.getPicture(options)
+      .then((imageData) => {
+        let base64image = 'data:image/jpeg;base64,' + imageData;
+        this.photo = base64image;
+      }, (error) => {
+        console.error(error);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+
+
+  }
+
+  ionViewDidLoad(){
+
+
+    this.geolocation.getCurrentPosition()
+      .then((resp) => {
+        this.resp_coords = resp.coords;
+        this.latitude = this.resp_coords.latitude;
+        this.longitude = this.resp_coords.longitude;
+
+        this.resp_coords = resp.coords;
+        console.log(resp);
+      }).catch((error) => {
+      console.log('Erro ao recuperar sua posição');
+      console.log(error);
+    });
+
+  }
 }
